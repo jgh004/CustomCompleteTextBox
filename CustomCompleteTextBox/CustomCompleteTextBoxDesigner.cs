3321738a9f2cc8bb49bc4de1195a066ed3a88666
IT nmg <jgh004@163.com>
2015-09-16 17:26:37 +0800

    修改设计时代码

--- CustomCompleteTextBox/CustomCompleteTextBoxDesigner.cs
+++ CustomCompleteTextBox/CustomCompleteTextBoxDesigner.cs
@@ -7,16 +7,25 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Windows.Forms.Design;
+using ExtLibrary;
 
 namespace CustomCompleteTextBox
 {
     public class CustomCompleteTextBoxDesigner : ControlDesigner
     {
+        private MessageFilter filter;
         private IComponentChangeService changeService = null;
 
         public override void Initialize( IComponent component )
         {
             base.Initialize( component );
+            filter = new MessageFilter();
+            filter.FilterMessages.Add(13);
+            filter.FilterMessageEvent += (o, eve ) =>
+            {
+                eve.StopMessage = true;
+            };
+            Application.AddMessageFilter( filter );
 
             this.changeService = this.GetService( typeof( IComponentChangeService ) ) as IComponentChangeService;
 
