133e5d0327b1db37b85c6e27a881206628f55cdf
IT nmg <jgh004@163.com>
2015-09-21 17:10:06 +0800

    修改 nuget 配置

--- CustomCompleteTextBox/Properties/AssemblyInfo.cs
+++ CustomCompleteTextBox/Properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle( "https://github.com/jgh004/CustomCompleteTextBox" )]
-[assembly: AssemblyDescription( "适用于 WinForm 的带下拉列表的自定义搜索文本框" )]
+[assembly: AssemblyTitle( "CustomCompleteTextBox" )]
+[assembly: AssemblyDescription( "适用于 WinForm 的带下拉列表的自定义搜索文本框控件, 支持自定义搜索条件." )]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany( "ITnmg" )]
 [assembly: AssemblyProduct("CustomCompleteTextBox")]
@@ -32,5 +32,6 @@ using System.Runtime.InteropServices;
 //可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值，
 // 方法是按如下所示使用“*”: :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion( "1.0.1.0" )]
-[assembly: AssemblyFileVersion( "1.0.1.0" )]
+[assembly: AssemblyVersion( "1.0.*" )]
+[assembly: AssemblyFileVersion( "1.0.2.0" )]
+[assembly: AssemblyInformationalVersion("1.0.2")]
